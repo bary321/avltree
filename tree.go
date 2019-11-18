c68c5683f31958eb6276c56f092e16ab7eaea292
bary321 <wyj13688@gmail.com>
2019-11-18 17:27:00 +0800

    代码调整

--- tree.go
+++ tree.go
@@ -1,35 +1,5 @@
 package avltree
 
-type BinaryNode struct {
-	left  Node
-	right Node
-	data  int64
-}
-
-func (bn *BinaryNode) GetData() int64 {
-	return bn.data
-}
-
-func (bn *BinaryNode) GetLeft() Node {
-	return bn.left
-}
-
-func (bn *BinaryNode) GetRight() Node {
-	return bn.right
-}
-
-func (bn *BinaryNode) SetData(data int64) {
-	bn.data = data
-}
-
-func (bn *BinaryNode) SetLeft(n Node) {
-	bn.left = n
-}
-
-func (bn *BinaryNode) SetRight(n Node) {
-	bn.right = n
-}
-
 type BinaryTree struct {
 	root Node
 }
