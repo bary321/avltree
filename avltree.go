044726ea88f2bbb8fc4716c8c83c26fdedfe1a79
bary321 <wyj13688@gmail.com>
2019-11-18 17:50:21 +0800

    提交

--- avltree.go
+++ avltree.go
@@ -5,6 +5,50 @@ type AVLTree struct {
 	root Node
 }
 
+func (at *AVLTree) Find(data int64) Node {
+	if at.root == nil {
+		return nil
+	}
+	tmp := at.root
+	for {
+		if tmp.GetData() == data {
+			t := tmp.(*AVLNode)
+			if !t.Del {
+				return tmp
+			}
+			return nil
+		} else if tmp.GetData() < data {
+			if tmp.GetRight() != nil {
+				tmp = tmp.GetRight()
+			} else {
+				return nil
+			}
+		} else {
+			if tmp.GetLeft() != nil {
+				tmp = tmp.GetLeft()
+			} else {
+				return nil
+			}
+		}
+	}
+}
+
+// todo: update
+func (at *AVLTree) FindMin() Node {
+	if at.root == nil {
+		return nil
+	}
+	tmp := at.root
+	for {
+		if tmp.GetLeft() != nil {
+			tmp = tmp.GetLeft()
+		} else {
+
+			return tmp
+		}
+	}
+}
+
 func (at *AVLTree) Insert(int64) {
 
 }
